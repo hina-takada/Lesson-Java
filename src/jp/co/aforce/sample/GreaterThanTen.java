84f76fe063db570ed93411e95ea0bc251b698daf
hina-takada <hina.takada@arsnv.co.jp>
2026-04-21 17:44:31 +0900

    2026.04.21 演習１完了

---
+++ src/jp/co/aforce/sample/GreaterThanTen.java
@@ -0,0 +1,14 @@
+package jp.co.aforce.sample;
+
+public class GreaterThanTen {
+
+	public static void main(String[] args) {
+		int num = 12;
+		if (num < 12) {
+			System.out.println("12より大きい");
+		}
+		System.out.println("12より小さい");
+
+	}
+
+}
