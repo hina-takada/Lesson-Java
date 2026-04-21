6f635e9117ce61169897cc8fd1f6e0f3804bae24
hina-takada <hina.takada@arsnv.co.jp>
2026-04-21 17:40:08 +0900

    2026.04.21 演習２完了

---
+++ src/jp/co/aforce/sample/EvenOrOdd.java
@@ -0,0 +1,105 @@
+package jp.co.aforce.sample;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		/*int num;*/
+		/*double num;*/
+		/*int num1;
+		int num2;*/
+		
+		/*16
+		 * double price= 12000;
+		double rate = 0.3;
+		double price2 = price * ()*/
+		
+		/*15
+		 * int[] tests = {75, 88,92};
+		int total = 0;
+		for(int test : tests) {
+			total += test;
+		}
+		System.out.println(total);
+		System.out.println(total / tests.length);*/
+		
+		
+		/*14
+		 * int num1 = 8;
+		int num2 = 3;
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);*/
+		
+		/*13
+		 * String name = "ラン";
+		int age = 20;
+		age++;
+		System.out.println(name + "は次の誕生日で" + age + "歳です");*/
+		
+		/*12
+		 * num1 = 6;
+		num2 = 7;
+		System.out.println(num1 * num2);*/
+		
+		/*11
+		 * num1 = 10;
+		num2 = 4;
+		System.out.println(num1 - num2);*/
+		
+		/*10
+		 * num1 = 8;
+		num2 = 3;
+		System.out.println(num1 + num2);*/
+		
+		
+		/*問題９
+		 * num = 15;
+		if(num == num % 5) {
+			System.out.println("5の倍数");
+		}else {
+			System.out.println("5の倍数ではない");
+		}*/
+		
+		/*問題８
+		 * num = -5;
+		if(num < 0) {
+			System.out.println("負の数である");
+		}else {
+			System.out.println("負の数ではない");
+		}*/
+		
+		
+		/*問題７
+		 * num = 0;
+		if(num == 0) {
+			System.out.println("0です");
+		}
+		System.out.println("0ではない");*/
+		
+		
+		/*10倍にして
+		 * num = 3.2;
+		System.out.println(num * 10.0);*/
+		
+		/*整数と小数　分ける
+		 * num = 7.8;
+		int value = (int)num;
+		double decimal = num - value;
+		System.out.println(value);
+		System.out.println(decimal);*/
+		
+		/*値を半分にする
+		 * double num = 4.5;
+		System.out.println(num / 2.0);*/
+		
+		/*偶数・奇数判定
+		 * int num = 7;
+		if (num == num % 2) {
+			System.out.println("偶数");
+		}
+		System.out.println("奇数");*/
+	}
+
+}
