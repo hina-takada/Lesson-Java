ead53bf804570de751506d9d66c77cf14443d832
hina-takada <hina.takada@arsnv.co.jp>
2026-04-22 15:45:20 +0900

    doubleをコメントしただけ

--- src/jp/co/aforce/sample/EvenOrOdd.java
+++ src/jp/co/aforce/sample/EvenOrOdd.java
@@ -4,15 +4,17 @@ public class EvenOrOdd {
 
 	public static void main(String[] args) {
 		/*int num;*/
-		/*double num;*/
+		double num;
 		/*int num1;
 		int num2;*/
-		
+
+
 		/*16
-		 * double price= 12000;
+		 * int price= 12000;
 		double rate = 0.3;
-		double price2 = price * ()*/
-		
+		double price2 = price * (1 - rate);
+		System.out.println(price2);*/
+
 		/*15
 		 * int[] tests = {75, 88,92};
 		int total = 0;
@@ -21,8 +23,7 @@ public class EvenOrOdd {
 		}
 		System.out.println(total);
 		System.out.println(total / tests.length);*/
-		
-		
+
 		/*14
 		 * int num1 = 8;
 		int num2 = 3;
@@ -31,37 +32,36 @@ public class EvenOrOdd {
 		System.out.println(num1 * num2);
 		System.out.println(num1 / num2);
 		System.out.println(num1 % num2);*/
-		
+
 		/*13
 		 * String name = "ラン";
 		int age = 20;
 		age++;
 		System.out.println(name + "は次の誕生日で" + age + "歳です");*/
-		
+
 		/*12
 		 * num1 = 6;
 		num2 = 7;
 		System.out.println(num1 * num2);*/
-		
+
 		/*11
 		 * num1 = 10;
 		num2 = 4;
 		System.out.println(num1 - num2);*/
-		
+
 		/*10
 		 * num1 = 8;
 		num2 = 3;
 		System.out.println(num1 + num2);*/
-		
-		
+
 		/*問題９
 		 * num = 15;
-		if(num == num % 5) {
+		if(num % 5 == 0) {
 			System.out.println("5の倍数");
 		}else {
 			System.out.println("5の倍数ではない");
 		}*/
-		
+
 		/*問題８
 		 * num = -5;
 		if(num < 0) {
@@ -69,34 +69,32 @@ public class EvenOrOdd {
 		}else {
 			System.out.println("負の数ではない");
 		}*/
-		
-		
+
 		/*問題７
 		 * num = 0;
 		if(num == 0) {
 			System.out.println("0です");
 		}
 		System.out.println("0ではない");*/
-		
-		
+
 		/*10倍にして
 		 * num = 3.2;
 		System.out.println(num * 10.0);*/
-		
+
 		/*整数と小数　分ける
 		 * num = 7.8;
 		int value = (int)num;
 		double decimal = num - value;
 		System.out.println(value);
 		System.out.println(decimal);*/
-		
+
 		/*値を半分にする
 		 * double num = 4.5;
 		System.out.println(num / 2.0);*/
-		
+
 		/*偶数・奇数判定
 		 * int num = 7;
-		if (num == num % 2) {
+		if (num % 2 == 0) {
 			System.out.println("偶数");
 		}
 		System.out.println("奇数");*/
