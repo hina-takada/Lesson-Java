06e944b3627e52ef2c8513cf07b4a5030a858f34
hina-takada <hina.takada@arsnv.co.jp>
2026-04-24 17:07:21 +0900

    研修：クラスmainメソッド

---
+++ src/jp/co/aforce/sample/Main.java
@@ -0,0 +1,16 @@
+package jp.co.aforce.sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Taiyaki taiyaki1 = new Taiyaki();
+		taiyaki1.setFilling("クリーム");
+		taiyaki1.showFilleing();
+		
+		Taiyaki taiyaki2 = new Taiyaki();
+		taiyaki2.setFilling("あんこ");
+		taiyaki2.showFilleing();
+
+	}
+
+}
