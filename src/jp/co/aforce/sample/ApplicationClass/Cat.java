17af3c60b12415afff841850f21523d5cb199d58
hina-takada <hina.takada@arsnv.co.jp>
2026-04-24 17:32:43 +0900

    Animalクラスが使用するクラス:オーバー

---
+++ src/jp/co/aforce/sample/ApplicationClass/Cat.java
@@ -0,0 +1,8 @@
+package jp.co.aforce.sample.ApplicationClass;
+
+public class Cat extends Animal{
+	@Override
+	void meleSound() {
+		System.out.println("にゃー");
+	}
+}
---
+++ src/jp/co/aforce/sample/ApplicationClass/Dog.java
@@ -0,0 +1,8 @@
+package jp.co.aforce.sample.ApplicationClass;
+
+public class Dog extends Animal {
+	@Override
+	void meleSound() {
+		System.out.println("わんわん");
+	}
+}
