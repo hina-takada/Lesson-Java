017782e042b9cbd31d4f983aa340ce0f836babe6
hina-takada <hina.takada@arsnv.co.jp>
2026-04-27 17:36:49 +0900

    10章：キャストに使用

---
+++ src/jp/co/aforce/sample/ApplicationClass/Rectangle.java
@@ -0,0 +1,33 @@
+package jp.co.aforce.sample.ApplicationClass;
+//10章：オーバーロード
+//10章：キャスト(インタフェ　Shape)
+public class Rectangle implements Shape {
+	private int width;
+	private int height;
+
+	public Rectangle(int width ,int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public Rectangle(int width) {
+		this.width = width;
+		this.height = width;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	//キャスト
+	@Override
+	public void calc() {
+		int product = height * width;
+		
+	}
+	
+}
