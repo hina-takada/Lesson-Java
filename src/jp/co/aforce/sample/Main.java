45465079fa5a495d28c1f664f854bb5463eb5983
hina-takada <hina.takada@arsnv.co.jp>
2026-04-27 17:30:08 +0900

    基礎⑦　静的フィールド追加

--- src/jp/co/aforce/sample/Main.java
+++ src/jp/co/aforce/sample/Main.java
@@ -3,13 +3,37 @@ package jp.co.aforce.sample;
 public class Main {
 
 	public static void main(String[] args) {
-		Taiyaki taiyaki1 = new Taiyaki();
+		/*基礎⑦　静的フィールド*/
+		Player p1 = new Player();
+		Player p2 = new Player();
+		
+		/*Player p1 = new Player(100);
+		Player p2 = new Player(100);*/
+		
+		System.out.println("初期状態:");
+		System.out.println("p1.hp = " + p1.hp1);//hp.p1
+		System.out.println("p2.hp = " + p2.hp2);
+		
+		//p1がダメージを受ける
+		
+		p1.hp1 -= 30;
+		/*hp.p1 -= 30;*/
+		
+		System.out.println("\nダメージ後:");
+		System.out.println("p1.hp = " + p1.hp1);
+		System.out.println("p2.hp = " + p2.hp2);
+		
+		/*インスタンス
+		 * Taiyaki taiyaki1 = new Taiyaki();
 		taiyaki1.setFilling("クリーム");
 		taiyaki1.showFilleing();
 		
 		Taiyaki taiyaki2 = new Taiyaki();
 		taiyaki2.setFilling("あんこ");
-		taiyaki2.showFilleing();
+		taiyaki2.showFilleing();*/
+		
+		
+		
 
 	}
 
