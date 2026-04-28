86b2d1486584d74b68b4742f968fb3e492b90ca9
hina-takada <hina.takada@arsnv.co.jp>
2026-04-28 17:19:28 +0900

    11章:名前空間

---
+++ src/com/example/school/Student.java
@@ -0,0 +1,23 @@
+package com.example.school;
+
+public class Student {
+	private String name;
+	private int grade;
+
+	public Student(String name,int grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void introduce() {
+		System.out.printf("名前は、%sです。学年は、%d年です。",name,grade);
+	}
+}
