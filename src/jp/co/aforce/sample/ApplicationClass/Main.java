5223da3dd2abf16886fa5ae54c4d7a9ce6ca3334
hina-takada <hina.takada@arsnv.co.jp>
2026-04-28 15:52:44 +0900

    10章：クラス応用（終了）

--- src/jp/co/aforce/sample/ApplicationClass/Main.java
+++ src/jp/co/aforce/sample/ApplicationClass/Main.java
@@ -5,6 +5,7 @@ import java.awt.Button;
 public class Main {
 	//10章：クラスの応用Main
 	public static void main(String[] args) {
+		//コンストラクター
 		Person person = new Person("名前", 10);
 		System.out.println(person.getName() + "：" +
 				person.getAge() + "歳です");
@@ -22,22 +23,28 @@ public class Main {
 		System.out.println(book2.toString());
 
 		System.out.println("=====================");
-
-		Dog dog = new Dog();
+		//オーバーライド
+		Animal dog = new Dog();
 		dog.meleSound();
-		Cat cat = new Cat();
+		Animal cat = new Cat();
 		cat.meleSound();
 
 		PersonOver perOver = new PersonOver("尾崎", 30);
-		System.out.println(perOver.toString());
+		System.out.println(perOver);
 
-		System.out.println("=====================");
-
-		Employee ep = new Employee(1);
-		System.out.println(ep.equals(ep));
+		System.out.println("-----------------------");
+		
+		Employee ep1 = new Employee(1);
+		Employee ep2 = new Employee(2);
+		Employee ep3 = new Employee(1);
+		
+		
+		System.out.println(ep1.equals(ep2));
+		System.out.println(ep1.equals(ep3));
 
 		System.out.println("=====================");
 		
+		//オーバーロード
 		System.out.println(add(2,4));
 		System.out.println(add(2,5,4));
 		
@@ -51,10 +58,12 @@ public class Main {
 		
 		System.out.println("---------------------");
 		//10章　問3
-		System.out.println(add(2,4,6,7));
+		int result = add(3,5,6,2,6);
+		System.out.println(result);
 		
 		System.out.println("=====================");
 		
+		//10章：無名内部クラス
 		InnerClass inner = new InnerClass();
 		Thread thread = new Thread(inner);
 		thread.start();
@@ -78,25 +87,83 @@ public class Main {
 		
 		System.out.println("----------------------");
 		
-		Shape circle = new Circle();
-		Shape rect = new Rectangle(2, 5);
+		Shape shape1 = new Circle();
+		Shape shape2 = new Rectangle(2, 5);
 		
-		System.out.println(circle instanceof Shape);
-		System.out.println(rect instanceof Shape);
+		if(shape1 instanceof Shape) {
+			Circle circle = (Circle)shape1;
+			circle.calc();
+		}
 		
-		circle.calc();
-		rect.calc();
+		if(shape2 instanceof Shape) {
+			Rectangle rect = (Rectangle)shape2;
+			rect.calc();
+		}
+		
 		
 		System.out.println("=====================");
 		//10章：静的フィールド
 		Counter counter1 = new Counter();
 		Counter counter2 = new Counter();
 		
-		counter1.add();
-		counter2.add();
+		System.out.println(Counter.count);
 		
 		System.out.println("----------------------");
 		
+		Student st1 = new Student("田中");
+		Student st2 = new Student("小林");
+		
+		System.out.println(st1.getName() + "は、" + Student.schoolName);
+		System.out.println(st2.getName() + "は、" + Student.schoolName);
+		
+		System.out.println(Configuration.appName);
+		System.out.println(Configuration.version);
+		
+		System.out.println("=====================");
+		//静的メソッド
+		System.out.println(MathUtils.add(3, 4));
+		
+		System.out.println("----------------------");
+		
+		System.out.println(StringUtils.toUpperCase("apple"));
+		
+		System.out.println("----------------------");
+		
+		int[] array = {3,5,6,7,3,40};
+		System.out.println(ArrayUtils.max(array));
+		
+		
+		System.out.println("=====================");
+		
+		MathConstants mc = new MathConstants();
+		double circleArea = Math.pow(3, 2) * mc.PI;
+		System.out.println(circleArea);
+		
+		System.out.println("----------------------");
+		
+		PhysicsConstants pc = new PhysicsConstants();
+		System.out.printf("%,d",pc.SPEED_OF_LIGHT * 1);
+		
+		System.out.println("\n----------------------");
+		
+		AppConfig ap = new AppConfig();
+		System.out.printf("アプリケーション名：%s,バージョン：%.1f",ap.APP_NAME ,ap.VERSION);
+		
+		System.out.println("\n=====================");
+		//列挙型
+		for(Day day : Day.values()) {
+			System.out.println(day);
+		}
+		System.out.println("----------------------");
+		
+		for(Season season : Season.values()) {
+			System.out.print(season);
+		}
+		System.out.println("\n----------------------");
+		
+		for(TrafficLight light : TrafficLight.values()) {
+			System.out.println(light + ":" + light.getMessage());
+		}
 		
 
 	}
@@ -110,8 +177,12 @@ public class Main {
 	}
 	
 	//10章　問3
-	static int add(int a, int b,int c,int d) {
-		return a + b + c + d;
+	static int add(int... numbers) {
+		int total = 0;
+		for(int number : numbers) {
+			total += number;
+		}
+		return total;
 	}
 
 }
