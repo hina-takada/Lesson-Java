f7a2a29edfc8f2c7c73d3a2b7c6506a78b1e8eb0
hina-takada <hina.takada@arsnv.co.jp>
2026-04-28 15:51:53 +0900

    10章：静的フィールド（問題1）

--- src/jp/co/aforce/sample/ApplicationClass/Counter.java
+++ src/jp/co/aforce/sample/ApplicationClass/Counter.java
@@ -1,9 +1,9 @@
 package jp.co.aforce.sample.ApplicationClass;
-//10章：静的メソッド
+//10章：静的メソッド(問題1)
 public class Counter {
-	static int count;
+	static int count = 0;
 	
-	void add() {
-		System.out.println(count++);
+	public Counter() {
+		count++;
 	}
 }
