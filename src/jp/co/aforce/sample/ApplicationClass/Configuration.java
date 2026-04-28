4f19952c503b51a1c7590ab8e2f45cc2ae6870b7
hina-takada <hina.takada@arsnv.co.jp>
2026-04-28 15:57:30 +0900

    10章：静的フィールド（問題3）

---
+++ src/jp/co/aforce/sample/ApplicationClass/Configuration.java
@@ -0,0 +1,12 @@
+package jp.co.aforce.sample.ApplicationClass;
+
+public class Configuration {
+	static String appName;
+	static int version;
+	
+	static {
+		appName = "アップ";
+		version = 0;
+	}
+	
+}
