1b6fd00f4db549a836e2456cddd36e5b194052f3
hina-takada <hina.takada@arsnv.co.jp>
2026-04-30 14:09:08 +0900

    11章：名前空間・スコープ

---
+++ src/com/example/utils/Helper.java
@@ -0,0 +1,8 @@
+package com.example.utils;
+
+public class Helper {
+	
+	void showMessage(String name , String message) {
+		System.out.println(name + ":" + message);
+	}
+}
---
+++ src/com/example/utils/Test.java
@@ -0,0 +1,8 @@
+package com.example.utils;
+
+public class Test {
+	public static void main(String[] args) {
+		Helper helper = new Helper();
+		helper.showMessage("○○さん", "メッセージ");
+	}
+}
